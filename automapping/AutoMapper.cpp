af89fb611f0e3558c71d0c77402b047a3575f69c
nFichin <nahue.fichin@gmail.com>
2013-03-29 11:50:47 -0300

    Nueva clase Automapper que administrara el cambios del mapa

---
+++ automapping/AutoMapper.cpp
@@ -0,0 +1,11 @@
+#include "AutoMapper.h"
+
+AutoMapper::AutoMapper() {
+	// TODO Auto-generated constructor stub
+
+}
+
+AutoMapper::~AutoMapper() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ automapping/AutoMapper.h
@@ -0,0 +1,10 @@
+#ifndef AUTOMAPPER_H_
+#define AUTOMAPPER_H_
+
+class AutoMapper {
+public:
+	AutoMapper();
+	virtual ~AutoMapper();
+};
+
+#endif /* AUTOMAPPER_H_ */
